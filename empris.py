3684e2aeb9600c60c11453790bd72178fc580280
Auric Vente <mprops@protonmail.com>
2021-09-25 17:49:12 -0600

    playerlist

--- empris.py
+++ empris.py
@@ -2,10 +2,28 @@ import os
 import sys
 from pyrofi import Rofi
 
-r = Rofi()
-players = []
-names = []
-labels = []
+class PlayerList:
+  def __init__(self):
+    self.players = []
+  
+  def add_player(self, player):
+    self.players.append(player)
+  
+  def get_names(self):
+    return [p.name for p in self.players]
+
+  def get_labels(self):
+    return [p.label for p in self.players]
+  
+  def get_playing(self):
+    playing = []
+    for i, player in enumerate(self.players):
+      if player.playing:
+        playing.append(i)
+    return playing
+  
+  def name(self, index):
+    return self.players[index].name
 
 class Player:
   def __init__(self, name):
@@ -21,50 +39,30 @@ class Player:
     self.label = label
 
 def get_players():
-  global players
-  global names
-  global labels
+  splist = os.popen("playerctl --list-all") \
+    .read().strip().split("\n")
 
-  players = []
-  names = []
-  labels = []
-
-  result = os.popen("playerctl --list-all").read().strip()
-  namelist = result.split("\n")
-
-  for name in namelist:
-    players.append(Player(name))
-
-  names = [p.name for p in players]
-  labels = [p.label for p in players]
-
-def get_playing():
-  playing = []
-
-  for i, player in enumerate(players):
-    if player.playing:
-      playing.append(i)
-      
-  return playing
+  for name in splist:
+    playerlist.add_player(Player(name))
 
 def show_menu():
   options = []
-  options += labels
+  options += playerlist.get_labels()
   options.append("---------")
   options.append("Pause All")
   options.append("Next Track")
   options.append("Prev Track")
 
   selected = 0
-  playing = get_playing()
+  playing = playerlist.get_playing()
   if len(playing) > 0:
     selected = playing[0]
 
   header = "Which player to play-pause"
-  index, key = r.select(header, options, select = selected, singleclick = True)
+  index, key = rofi.select(header, options, select = selected, singleclick = True)
 
   if (key == 0):
-    if index < len(labels):
+    if index < len(playerlist.players):
       pause_all_except(index)
     else:
       if options[index] == "Pause All":
@@ -75,37 +73,40 @@ def show_menu():
         go_prev()
 
 def play_pause(index):
-  os.popen(f"playerctl -p {names[index]} play-pause")
+  os.popen(f"playerctl -p {playerlist.name(index)} play-pause")
 
 def play(index):
-  os.popen(f"playerctl -p {names[index]} play")
+  os.popen(f"playerctl -p {playerlist.name(index)} play")
 
 def pause(index):
-  player = players[index]
+  player = playerlist.players[index]
   if player.playing:
-    os.popen(f"playerctl -p {names[index]} pause")
+    os.popen(f"playerctl -p {playerlist.name(index)} pause")
 
 def pause_all_except(index):
   pause_all()
   play(index)
 
 def pause_all():
-  for i, player in enumerate(players):
+  for i, player in enumerate(playerlist.players):
     pause(i)
 
 def go_next():
-  for player in players:
+  for player in playerlist.players:
     if player.playing:
       os.popen(f"playerctl -p {player.name} next")
       return
 
 def go_prev():
-  for player in players:
+  for player in playerlist.players:
     if player.playing:
       os.popen(f"playerctl -p {player.name} previous")
       return
 
 if (__name__ == "__main__"):
+  rofi = Rofi()
+  playerlist = PlayerList()
+
   mode = ""
   if len(sys.argv) > 1:
     mode = sys.argv[1]
